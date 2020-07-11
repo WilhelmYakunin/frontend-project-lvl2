b8aec45dbbdc1807259b4d6275668d4143805029
WilhelmYakunin <vit.yakunin@gmail.com>
2020-07-12 00:44:42 +0300

    Roboto in catalog

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ const program = require('commander');
 
 program
     .version('1.0.0') // alternative sub-command is `al`
-    .description('Compares two configuration files and shows a difference.');
-
+    .description('Compares two configuration files and shows a difference.')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format [type]', 'output format');
+    
     program.parse(process.argv);
